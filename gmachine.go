479aeebd31bd25fac2e4f0b70d1f743692ae09d1
VÖRÖSKŐI András <voroskoi@gmail.com>
2020-12-24 20:34:03 +0100

    Add Gmachine struct, and New() fucntion

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,16 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+// Gmachine represents a G-machine.
+type Gmachine struct {
+	P      uint64
+	Memory []uint64
+}
+
+// New creates and initializes a new G-machine.
+func New() *Gmachine {
+	gm := Gmachine{}
+	gm.Memory = make([]uint64, DefaultMemSize)
+	return &gm
+}
