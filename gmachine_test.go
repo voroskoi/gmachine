243df518f54368bc2d357412bd1a6e292af72527
VÖRÖSKŐI András <voroskoi@gmail.com>
2020-12-25 22:43:45 +0100

    Test: Solve 3-2 with SETA

--- gmachine_test.go
+++ gmachine_test.go
@@ -101,3 +101,17 @@ func TestSETA(t *testing.T) {
 		t.Errorf("program counter should be %d, got %d", wantP, gm.P)
 	}
 }
+
+func TestSubt32(t *testing.T) {
+	t.Parallel()
+	gm := gmachine.New()
+	gm.Memory[0] = gmachine.OpSETA
+	gm.Memory[1] = 3
+	gm.Memory[2] = gmachine.OpDECA
+	gm.Memory[3] = gmachine.OpDECA
+	gm.Run()
+	wantA := uint64(1)
+	if gm.A != wantA {
+		t.Errorf("OpSETA 3; OpDECA, OpDECA should be %d, got %d", wantA, gm.A)
+	}
+}
